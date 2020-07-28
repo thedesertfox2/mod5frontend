6929ce78062844a5bcc4218df9438a5d63d4c05b
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-07-28 15:19:19 -0400

    lkjasdf

--- src/component/DrivingLog.js
+++ src/component/DrivingLog.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactPlayer from 'react-player/youtube'
+import Maneuvers from './Maneuvers'
 
 class DrivingLog extends React.Component {
 
     state = {
-        maneuvers: []
+        maneuvers: [],
+        renderManeuvers: false
     }
 
     componentDidMount(){
@@ -14,16 +16,67 @@ class DrivingLog extends React.Component {
             maneuvers: data
         }))
     }
+    
+    renderManeuvers = () => {
+        this.setState({
+            renderManeuvers: !this.state.renderManeuvers
+        })
+    } 
 
     render(){
         return(
             <div>
-                {this.state.maneuvers.map(man => 
+                {this.state.renderManeuvers ? <div>{this.state.maneuvers.map(man => <Maneuvers maneuverObj={man} key={man.id}/>) }<input type='button' onClick={this.renderManeuvers} value='Return to Driving Log Page'/></div>:
+                <div>
                     <div>
-                        <h4>{man.name}</h4>
-                        <ReactPlayer url={man.url} />
+                        <h4>Controls, Gauges, and Maintenance (at least 2hrs.) </h4>
+                        <h6>Objective:</h6> <p>Student should know how to operate the pedals, switches, buttons, and other vehicle controls.</p>
+                        <br/>
                     </div>
-                )}
+
+                    <div>
+                        <h4>Learning the Basics (at least 2 hrs.)</h4>
+                        <h6>Objective:</h6> <p>Student should be able to start, stop, drive forward, reverse, and turn vehicle.</p>
+                        <br/>
+                    </div>
+
+                    <div>
+                        <h4>Driving in Limited Traffic (at least 6 hrs.)</h4>
+                        <h6>Objective:</h6> <p>Student should be able to drive on lightly-traveled, two lane residential streets or country roads not over 35MPH.</p>
+                        <br/>
+                    </div>
+
+                    <div onClick={this.renderManeuvers}>
+                        <h4>Parking (at least 2hrs.)</h4>
+                        <h6>Objective:</h6> <p>Student Should be able to park in a lot with cars parked and moving around.</p>
+                        <br/>
+                    </div>
+
+                    <div>
+                        <h4>Driving on Limited-Access Highways (at least 6hrs.)</h4>
+                        <h6>Objective:</h6> <p>Student should be able to drive during non-rush hour time.</p>
+                        <br/>
+                    </div>
+                    
+                    <div>
+                        <h4>Handling the Vehicle in Traffic (at least 8hrs.)</h4>
+                        <h6>Objective:</h6> <p>Student should be able to move onto routes with a mixture of neighborhood streets and commercial areas, including 4 lane roads and designated turn lanes.  Speed limit should not exceed 40MPH.</p>
+                        <br/>
+                    </div>
+
+                    <div>
+                        <h4>Driving at Night (at least 5hrs.)</h4>
+                        <h6>Objective:</h6> <p>Student should be able to drive safely at night.</p>
+                        <br/>
+                    </div>
+
+                    <div>
+                        <h4>Additional Practice (at least 8hrs.)</h4>
+                        <h6>Objective:</h6> <p>Student should be able to drive you on errands and trips, following directions and route themselves.</p>
+                        <br/>
+                    </div>
+                </div>
+                }
             </div>
         )
     }
--- src/component/Maneuvers.js
+++ src/component/Maneuvers.js
@@ -1,61 +1,22 @@
 import React from 'react'
-import Pil from '../videos/P-I-L.mp4'
-import PilPicture from '../images/pilpicture.webp'
-import PirPicture from '../images/pirpicture.webp'
-import Pir from '../videos/P-I-R.mp4'
-import Bil from '../videos/B-I-L.mp4'
-import BilPicture from '../images/bilpicture.webp'
-import Bir from '../videos/B-I-R.mp4'
-import BirPicture from '../images/birpicture.webp'
-import Parallel from '../videos/parallel.mp4'
-import ParallelPicture from '../images/parallel.webp'
-import ThreePoint from '../videos/threepoint.mp4'
-import ThreePointPicture from '../images/threepoint.webp'
 import ReactPlayer from 'react-player/youtube'
 
 
 class Maneuvers extends React.Component {
+    state = {
+        renderManeuvers: false
+    }
+
+    renderManeuvers = () => {
+        this.setState({
+            renderManeuvers: !this.state.renderManeuvers
+        })
+    }
+
     render(){
         return(
             <div>
-                <ReactPlayer url='https://www.youtube.com/watch?v=uG0haUpAEX4'/>
-
-
-                <video width='320' height='240' controls poster={PilPicture}>
-                    <source url='https://www.youtube.com/watch?v=uG0haUpAEX4' type='video/mp4' /> 
-                </video>
-                <p>Pull in Parking on the left</p>
-                <br/>
-
-                <video width='320' height='240' controls poster={PirPicture}>
-                    <source src={Pir} type='video/mp4' /> 
-                </video>
-                <p>Pull in Parking on the Right</p>
-                <br/>
-
-                <video width='320' height='240' controls poster={BilPicture}>
-                    <source src={Bil} type='video/mp4' /> 
-                </video>
-                <p>Back in Parking on the Left</p>
-                <br/>
-
-                <video width='320' height='240' controls poster={BirPicture}>
-                    <source src={Bir} type='video/mp4' /> 
-                </video>
-                <p>Back in Parking on the Right</p>
-                <br/>
-
-                <video width='320' height='240' controls poster={ParallelPicture}>
-                    <source src={Parallel} type='video/mp4' /> 
-                </video>
-                <p>Parallel Parking</p>
-                <br/>
-
-                <video width='320' height='240' controls poster={ThreePointPicture}>
-                    <source src={ThreePoint} type='video/mp4' /> 
-                </video>
-                <p>Three Point Turns</p>
-                <br/>
+                {this.state.renderManeuvers ? <div onClick={this.renderManeuvers}><h3>{this.props.maneuverObj.name}</h3><ReactPlayer url={this.props.maneuverObj.url}/></div> : <div onClick={this.renderManeuvers}><h3>{this.props.maneuverObj.name}</h3></div>}
             </div>
         )
     }
